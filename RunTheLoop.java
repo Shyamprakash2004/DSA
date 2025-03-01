ea255e9be15dcf68f981205f85ca2d7dfa4206ab
Shyamprakash2004 <139210194+Shyamprakash2004@users.noreply.github.com>
2025-03-01 09:05:00 +0530

    Create RunTheLoop.java

---
+++ RunTheLoop.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int i;
+        for(i=2;i<=n;i++){
+            System.out.print(i+"\t");
+        }
+    }
+}
